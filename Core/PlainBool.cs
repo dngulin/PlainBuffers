0ad1dcdaddf2752ac46c2425f61224e86c907b7b
Danil Gulin <dngulin@yandex.ru>
2020-09-17 20:44:11 +0300

    Add test for the `PlainBool` API and fix some bugs

--- Core/PlainBool.cs
+++ Core/PlainBool.cs
@@ -4,6 +4,9 @@ namespace PlainBuffers.Core {
   public readonly ref struct PlainBool {
     public const int Size = sizeof(byte);
 
+    private const byte True = 1;
+    private const byte False = 0;
+
     // ReSharper disable once InconsistentNaming
     public readonly Span<byte> _Buffer;
 
@@ -14,13 +17,13 @@ namespace PlainBuffers.Core {
       _Buffer = buffer;
     }
 
-    public unsafe bool Read() => *(bool*) _Buffer[0];
-    public unsafe void Write(bool value) => _Buffer[0] = *(byte*) &value;
+    public bool Read() => _Buffer[0] != False;
+    public void Write(bool value) => _Buffer[0] = value ? True : False;
 
     public void CopyTo(PlainBool other) => _Buffer.CopyTo(other._Buffer);
 
-    public static bool operator ==(PlainBool l, PlainBool r) => l._Buffer == r._Buffer;
-    public static bool operator !=(PlainBool l, PlainBool r) => l._Buffer != r._Buffer;
+    public static bool operator ==(PlainBool l, PlainBool r) => l._Buffer.SequenceEqual(r._Buffer);
+    public static bool operator !=(PlainBool l, PlainBool r) => !l._Buffer.SequenceEqual(r._Buffer);
 
     public override bool Equals(object obj) => false;
     public override int GetHashCode() => throw new NotSupportedException();
